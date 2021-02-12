84b4eef0e6968a2931ee8e2d0b96709b275a1b95
hitohito1023 <kinaseryota@kinazeryoutanoMacBook-Pro.local>
2021-02-12 17:28:29 +0900

    app.js作成

--- quiz_app/app.js
+++ quiz_app/app.js
@@ -1,38 +1,10 @@
-<!doctype html>
-<html class="no-js" lang="">
+const question = "ゲーム市場、もっとも売れたゲーム機は次のうちどれ？";
+const answers = [
+  "スーパーファミコン",
+  "プレイステーション",
+  "ニンテンドースイッチ",
+  "ニンテンドーDS"
+];
+const correct = "ニンテンドーDS";
 
-<head>
-  <meta charset="utf-8">
-  <title></title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <link rel="manifest" href="site.webmanifest">
-  <link rel="apple-touch-icon" href="icon.png">
-  <!-- Place favicon.ico in the root directory -->
-
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-
-  <meta name="theme-color" content="#fafafa">
-</head>
-
-<body>
-
-  <div class="container">
-
-    <div id="js-question" class="mt-3 alert alert-primary" role="alert">
-      A simple primary alert-check it out!
-    </div>
-
-    <div class="d-flex justify-content-center">
-      <button type="button" class="btn btn-primary">Primary</button>
-      <button type="button" class="ml-1 btn btn-primary">Primary</button>
-      <button type="button" class="ml-1 btn btn-primary">Primary</button>
-      <button type="button" class="ml-1 btn btn-primary">Primary</button>
-    </div>
-    
-  </div>
-
-</body>
-
-</html>
+document.getElementById('js-question').textContent = question;
