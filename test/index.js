aab1f189d87a45359ef7b9b23f4d71cff0dc9404
hitohito1023 <kinaseryota@kinazeryoutanoMacBook-Pro.local>
2021-02-12 18:55:26 +0900

    関数

---
+++ test/index.js
@@ -0,0 +1,30 @@
+let gree = 'Hello World!';
+
+// gree = 'Hello World2!!';
+
+const tool = "He..Hell...Hello World!";
+
+let array = ['いち', 'に', 'san','yon'];
+
+
+// let index = 0;
+// while(index < array.length){
+//   console.log(array[index]);
+//   index++;
+// }
+
+// if (array.length > 5) {
+//   console.log('aiueo');
+// } else {
+//   console.log('kakikukeko');
+// }
+
+const test = () => {
+  if (array.length > 5) {
+    console.log('aiueo');
+  } else {
+    console.log('kakikukeko');
+  }
+};
+
+test();
