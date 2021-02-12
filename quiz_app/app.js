de7002406c65a5fa841f213c2353437363214a05
hitohito1023 <kinaseryota@kinazeryoutanoMacBook-Pro.local>
2021-02-12 17:58:20 +0900

    ボタン作成

--- quiz_app/app.js
+++ quiz_app/app.js
@@ -7,4 +7,34 @@ const answers = [
 ];
 const correct = "ニンテンドーDS";
 
-document.getElementById('js-question').textContent = question;
+const $button = document.getElementsByTagName('button');
+
+const setupQuiz = () => {
+  document.getElementById('js-question').textContent = question;
+  let buttonIndex = 0;
+  let buttonLength = $button.length;
+  while(buttonIndex < buttonLength){
+    $button[buttonIndex].textContent = answers[buttonIndex];
+    buttonIndex++;
+  }
+}
+setupQuiz();
+
+const clickHandler = (e) => {
+  if(correct === e.target.textContent){
+    window.alert('正解！');
+  }else{
+    window.alert('不正解！');
+  }
+};
+
+let handlerIndex = 0;
+const buttonLength = $button.length;
+while (handlerIndex < buttonLength) {
+  $button[handlerIndex].addEventListener('click', (e) => {
+    clickHandler(e);
+  });
+  handlerIndex++;
+}
+
+
